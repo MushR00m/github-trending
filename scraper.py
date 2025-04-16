686c6365b96ca489baa0c8781f723611a35e83ba
MushR00m <2500005+MushR00m@users.noreply.github.com>
2025-04-15 23:11:48 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -31,7 +31,7 @@ def scrape(language, filename):
         'Accept-Language'	: 'zh-CN,zh;q=0.8'
     }
 
-    url = 'https://github.com/trending/{language}'.format(language=language)
+    url = 'https://github.com/trending/{language}?since=daily'.format(language=language)
     r = requests.get(url, headers=HEADERS)
     assert r.status_code == 200
     
